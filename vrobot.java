77fae0496986f9a88665881045544e70d815640a
vladimir0ne1@gmail.com <vladimir0ne1@gmail.com>
2012-02-25 19:34:16 +0300

    Takeover target done

--- vrobot.java
+++ vrobot.java
@@ -55,28 +55,46 @@ public class vrobot extends AdvancedRobot {
 		System.out.println("Heading: " + getHeading());		
 		//setTurnRight(0);
 		System.out.println("sin30: " + Math.sin(30*(Math.PI/180)));
-		System.out.println("Absolute (-30) : " + Utils.normalAbsoluteAngleDegrees(-30));
-		//System.out.println("Relative (-30) : " + Utils.normalAbsoluteAngleDegrees(-30));
+		System.out.println("Relative (210) : " + Utils.normalAbsoluteAngleDegrees(-150));
+		System.out.println("Relative (210) : " + Utils.normalRelativeAngleDegrees(210));
+		System.out.println("Relative (330) : " + Utils.normalRelativeAngleDegrees(330));
 		while(true) {			
 			turnRadarRightRadians(rt);
 
 		}
 	}
+	
+	public double getBearingWithSign(double heading, double bearing)
+	{
+		if(heading > bearing)
+			return -bearing;
+		else
+			return bearing;
+	}
+	
 
 	/**
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	//@SuppressWarnings("deprecation")
 	public void onScannedRobot(ScannedRobotEvent e) {
+		double enemyHeading = Utils.normalAbsoluteAngleDegrees(getHeading() + e.getBearing());
 		
-		double radarTurn =
-		        // Absolute bearing to target
-		        getHeadingRadians() + e.getBearingRadians()
-		        // Subtract current radar heading to get turn required
-		        - getRadarHeadingRadians();
-		System.out.println("e.RadarBearing: " + radarTurn + "\nabcdd: " + getRadarHeadingRadians());
-		double normalBearing=getNormalBearing(e.getBearing());
-		double normalNewRadarHeading = getNormalHeading(normalBearing, getHeading());
+		double radarTurn = enemyHeading - getRadarHeading();
+		//					+ getBearingWithSign(getHeading(), Utils.normalAbsoluteAngleDegrees(e.getBearing()))
+		//					- getRadarHeading();
+		//System.out.println("\n=== onScanned ====\nradarTurn " + radarTurn + "\ngetRadarHeading: " + getRadarHeading()
+		//			+ "\nUtils.normalAbsoluteAngleDegrees(e.getBearing())" + Utils.normalAbsoluteAngleDegrees(e.getBearing())
+		//			+ "\ngetHeading" + getHeading());
+		
+		System.out.println("enemyHeading: " + enemyHeading);
+		System.out.println("\nradarTurn: " + radarTurn);
+		setTurnRadarRight(radarTurn);
+		
+		setAhead(5);
+		setTurnRight(100);
+		//setAhead(-5);
+		//execute();
 		/*if (e.getBearing()!= -90)
 			{
 				System.out.println("e.Bearing: " + e.getBearing() + "\nHeading: " + getHeading() + "\nGunHeading " + getGunHeading());
@@ -92,7 +110,7 @@ public class vrobot extends AdvancedRobot {
 		//ahead(50);
 		//setTurnLeft(50);
 		//turnRight(5);
-		setTurnRadarRight((normalNewRadarHeading - getRadarHeading())*2);
+		//setTurnRadarRight(radarTurn*2);
 		//getRadar
 		
 		fire(3);
@@ -149,6 +167,6 @@ public class vrobot extends AdvancedRobot {
 	 */
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
-		back(300);
+		back(100);
 	}	
 }
