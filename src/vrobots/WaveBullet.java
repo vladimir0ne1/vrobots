83386d0b49b6972475d4d7635c4d09c7a82c3448
Vladimir <vladimir0ne1@gmail.com>
2012-06-04 18:00:34 +0300

    add Coments

--- src/vrobots/WaveBullet.java
+++ src/vrobots/WaveBullet.java
@@ -6,12 +6,21 @@ import robocode.util.Utils;
 
 class WaveBullet
 {
-	//123
 	private double startX, startY, startBearing, power;
 	private long   fireTime;
 	private int    direction;
 	private int[]  returnSegment;
  
+	/**
+	 * 
+	 * @param x	Первоначальные координаты (откуда произведен выстрел) (X)
+	 * @param y Первоначальные координаты (откуда произведен выстрел) (Y)
+	 * @param bearing Направление на врага (абсолютное)
+	 * @param power Мощность выстрела
+	 * @param direction Направление движения врага (по часовой (1), против (-1))
+	 * @param time Время выстрела
+	 * @param segment Массив индексов (наибольший индекс соответствует наилучшему GaussFactor)
+	 */
 	public WaveBullet(double x, double y, double bearing, double power,
 			int direction, long time, int[] segment)
 	{
@@ -23,32 +32,64 @@ class WaveBullet
 		fireTime       = time;
 		returnSegment  = segment;
 	}
-	
+	/** Вычисление скорости пули
+	 * 
+	 * @return 20 - power * 3;
+	 */
 	public double getBulletSpeed()
 	{
+		/** power - мощность снаряда (1..3)
+		 * 20 - максимальный порог скорости
+		 */
 		return 20 - power * 3;
 	}
  
+	/** Вычисление максимальный угла отклонения цели.
+	 * 
+	 * @return Math.asin(8 / getBulletSpeed());
+	 */
 	public double maxEscapeAngle()
 	{
+		/** Рассчитывает максимальный угол отклонения цели.
+		 * Треугольник. Стреляем из точки А в точку С.
+		 *	b = D (расстояние между нашим роботом и роботом врага)
+		 * c = Vb * t (Vb - скорость пули)
+		 * a = Vr * t (Vr скорость вражеского робота)
+		 * a/sin(A) = c/sin(C) A = asin(Vr/Vb * sin(C))
+		 * sin(c) в худшем случае равен 1
+		 * Максимальная скорость робота равна 8
+		 * Получаем: A = asin(8/vb)
+		 * 
+		 */
 		return Math.asin(8 / getBulletSpeed());
 	}
 	
+	/** Проверяем, попал ли какой-либо из наших снарядов в цель
+	 * 
+	 * @param enemyX Координаты врага (X)
+	 * @param enemyY Координаты врага (Y)
+	 * @param currentTime Текущее время
+	 * @return false, если мимо, true - иначе
+	 */
 	public boolean checkHit(double enemyX, double enemyY, long currentTime)
 	{
-		// if the distance from the wave origin to our enemy has passed
-		// the distance the bullet would have traveled...
+		// Если расстояние, пройденное пулей, больше, чем расстояние до врага - промах
 		if (Point2D.distance(startX, startY, enemyX, enemyY) <= 
 				(currentTime - fireTime) * getBulletSpeed())
 		{
+			// Угол, на который сместился враг относительно начального положения
 			double desiredDirection = Math.atan2(enemyX - startX, enemyY - startY);
+			// Смещение относительно первоначального угла и угла-смещения
 			double angleOffset = Utils.normalRelativeAngle(desiredDirection - startBearing);
+			// Считаем GaussFactor
 			double guessFactor =
 				Math.max(-1, Math.min(1, angleOffset / maxEscapeAngle())) * direction;
+			// Считаем индекс
 			int index = (int) Math.round((returnSegment.length - 1) /2 * (guessFactor + 1));
+			// Увеличиваем значение по полученному индексу. Максимальное значение соответствует лучшему GaussFactor
 			returnSegment[index]++;
 			return true;
 		}
 		return false;
 	}
-} // end WaveBullet class
+}
